daaf3e051d1ec9f2d6d03b982aab06f0d8ef11c7
ShAMURAI888 <stha0083@student.monash.edu>
2026-01-02 13:44:55 +0300

    Create training loop structure

---
+++ ModelTrainingEpoch.py
@@ -0,0 +1,50 @@
+import torch
+from torch import nn
+from torch import device
+from torch.optim import Optimizer
+from torch.utils.data import DataLoader
+
+def model_training_epoch(model : nn.Module,
+                         training_data_loader : DataLoader,
+                         loss_function : nn.Module,
+                         optimizer : Optimizer,
+                         device : device):
+    """
+    Docstring for model_training_epoch
+
+    :param model: The neural network to be trained
+    :type model: nn.Module
+    :param training_data_loader: The data loader module which loads the data in batches when required
+    :type training_data_loader: DataLoader
+    :param loss_function: Loss function for calculating the loss by comparing prediction and expected output
+                          loss.backward() performs backpropagation
+    :type loss_function: nn.Module (parent base class for loss functions)
+    :param optimizer: To perform corrections according to calculated loss to get better outputs
+                      Adjusts weights and biases during training to minimize the loss function
+    :type optimizer: Optimizer
+    :param device: The device on which the model training takes place
+                   (cuda cores in nvidia gpu (if available) OR cpu)
+    :type device: device
+    """
+    model.to(device=device)
+    model.train()
+    total_epoch_loss = 0.0
+
+    for input_batch, output_batch in training_data_loader:
+        input_batch, output_batch = input_batch.to(device), output_batch.to(device)
+        current_batch_loss = 0.0
+
+        optimizer.zero_grad()
+        predictions = model(input_batch)
+        loss = loss_function(predictions, output_batch)
+        loss.backward()
+        optimizer.step()
+
+        current_batch_loss += abs(loss.item())
+        average_batch_loss = current_batch_loss/len(input_batch)
+        #can print avg batch loss if needed
+        total_epoch_loss += average_batch_loss
+
+    average_epoch_loss = total_epoch_loss/len(training_data_loader)
+    return average_epoch_loss
+
